908d1887a3aae7c1c0d0a489987187bc99c26f1a
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2022-11-21 00:19:52 +0100

    add mocha basic example

---
+++ mocha-basic/test/my-test.js
@@ -0,0 +1,5 @@
+describe('bar', () => {
+  it('foo', () => {
+    console.log('foo');
+  });
+});
---
+++ mocha-basic/test/test.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+describe(`Example`, () => {
+  it('should pass', () => {});
+  it('should fail', () => {
+    assert.equal(1, 2);
+  });
+});
