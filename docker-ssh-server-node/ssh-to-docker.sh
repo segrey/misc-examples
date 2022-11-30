590a4d90cda8e2afd0ba930a13c95e96e0707155
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2022-12-01 00:05:05 +0100

    add port forwarding example

--- docker-ssh-server-node/ssh-to-docker.sh
+++ docker-ssh-server-node/ssh-to-docker.sh
@@ -6,3 +6,5 @@ set -e
 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ssh-server-node
 
 ssh my_user@localhost -p 2222
+
+#ssh my_user@localhost -p 2222 -L 3000:127.0.0.1:3000
