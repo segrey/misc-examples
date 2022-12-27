af924ba9f09b211ef82349a7d7d0c8a0b920f46e
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2022-12-27 13:10:20 +0100

    use 127.0.0.1 instead of localhost

--- docker-ssh-server-node/ssh-to-docker.sh
+++ docker-ssh-server-node/ssh-to-docker.sh
@@ -5,6 +5,6 @@ set -e
 
 docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ssh-server-node
 
-ssh my_user@localhost -p 2222
+ssh my_user@127.0.0.1 -p 2222
 
-#ssh my_user@localhost -p 2222 -L 3000:127.0.0.1:3000
+#ssh my_user@127.0.0.1 -p 2222 -L 3000:127.0.0.1:3000
