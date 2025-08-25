e4cd08b8aa92baf83ef1fd13dea9e8aefed863c8
Sergey Simonchik <sergey.simonchik@jetbrains.com>
2025-08-25 11:20:50 +0200

    another ssh server inside Docker

---
+++ docker-ssh-server-node/server2/ssh-to-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -x
+set -e
+
+docker inspect --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' ssh-server-node
+
+ssh my_user@localhost -p 2222
+
+#ssh my_user@localhost -p 2222 -L 3000:127.0.0.1:3000
